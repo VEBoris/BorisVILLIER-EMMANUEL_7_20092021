41c3bdd5c53b7ee9c41ac9764f1c5f114a144121
VEBoris <Visuya@outlook.fr>
2022-03-31 03:17:06 +0200

    add login signup

---
+++ frontend/src/pages/Logs/components/LogMenu.js
@@ -0,0 +1,23 @@
+import "./LogMenu.scss";
+import { LinkContainer } from "react-router-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
+
+function LogMenu() {
+  return (
+    <Navbar sticky="top" bg="light" expand="md" id="log-menu">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <LinkContainer to={`/signup`}>
+            <Nav.Link> Inscription </Nav.Link>
+          </LinkContainer>
+          <LinkContainer to={`/`}>
+            <Nav.Link> Connexion </Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default LogMenu;
