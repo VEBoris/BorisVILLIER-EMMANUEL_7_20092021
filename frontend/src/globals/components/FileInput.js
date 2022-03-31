c150406f6bc0ca3bfe0a32919dc4e98272a47e1a
VEBoris <Visuya@outlook.fr>
2022-03-31 03:12:23 +0200

    add globals component

---
+++ frontend/src/globals/components/FileInput.js
@@ -0,0 +1,41 @@
+import "./FileInput.scss";
+
+function FileInput(props) {
+  const handleChange = (event) => {
+    props.setNewFile(event.target.files[0]);
+  };
+
+  return (
+    <>
+      <form
+        action="/"
+        method="post"
+        encType="multipart/form-data"
+        target="_self"
+      >
+        <input
+          type="file"
+          name="media"
+          id="photos-input"
+          accept={props.accept}
+          className="file-input"
+          onChange={(event) => handleChange(event)}
+        />
+        <label
+          className={
+            props.newFile && props.newFile.name.length > 9
+              ? `${props.class} file-label long-file`
+              : `${props.class} file-label`
+          }
+          htmlFor="photos-input"
+        >
+          {props.newFile
+            ? `${props.newFile.name.substr(0, 13)} ...`
+            : props.text}
+        </label>
+      </form>
+    </>
+  );
+}
+
+export default FileInput;
