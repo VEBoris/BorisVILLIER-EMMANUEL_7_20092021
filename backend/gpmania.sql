8212de9fca8d42c20333b5d1566cefb7a17d494c
VEBoris <Visuya@outlook.fr>
2022-03-31 03:08:20 +0200

    add sql file

---
+++ backend/gpmania.sql
@@ -0,0 +1,172 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `gpmania`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `text` text NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `postId` int(11) DEFAULT NULL,
+  `userId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `comments`
+--
+
+INSERT INTO `comments` (`id`, `text`, `createdAt`, `updatedAt`, `postId`, `userId`) VALUES
+(22, 'Yo ! ', '2021-12-15 20:47:47', '2021-12-15 20:47:47', NULL, 1),
+(26, 'excellent en vrai !', '2021-12-16 22:48:34', '2021-12-16 22:48:34', 16, 1),
+(27, 'super !', '2021-12-16 09:47:29', '2021-12-16 09:47:29', 23, 1),
+(32, 'Incroyable !', '2021-12-16 13:58:49', '2021-12-16 13:58:49', 25, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `mediaUrl` varchar(255) DEFAULT NULL,
+  `likes` int(11) NOT NULL DEFAULT 0,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `userId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `company_role` varchar(255) NOT NULL,
+  `profile_picture` varchar(255) DEFAULT 'http://localhost:1331/medias/default_picture.jpg',
+  `user_role` varchar(255) NOT NULL DEFAULT 'guest'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `firstname`, `lastname`, `company_role`, `profile_picture`, `user_role`) VALUES
+(1, 'adminTest@gmail.com', '$2b$12$i0TxAEQ2NaXOEONQGuxUku6Ys4Pzfvyta7aR6rXpf.2UnVWXfuOeO', 'Admin', 'User', 'Administrateur', 'http://localhost:1331/medias/174039552_487529148953751_1159904828082402994_n.jpg1618752263261.jpg', 'admin'),
+(6, 'test@gmail.com', '$2b$12$M1Y5H7D7spNFvy.2XIQqkuguo/AnuA9yJw9He24wGOnIbVUGFulfS', 'Boris', 'VE', 'Devloppeur', 'http://localhost:1331/medias/173454540_1116791688794323_1173536189679175181_n.jpg1619788764398.jpg', 'guest');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `usersliked`
+--
+
+CREATE TABLE `usersliked` (
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `userId` int(11) NOT NULL,
+  `postId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `postId` (`postId`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Index pour la table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Index pour la table `usersliked`
+--
+ALTER TABLE `usersliked`
+  ADD PRIMARY KEY (`userId`,`postId`),
+  ADD KEY `postId` (`postId`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT pour la table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`postId`) REFERENCES `posts` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `usersliked`
+--
+ALTER TABLE `usersliked`
+  ADD CONSTRAINT `usersliked_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `usersliked_ibfk_2` FOREIGN KEY (`postId`) REFERENCES `posts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
