79d194f107d305427a2aa8054a445e8617c0d33e
VEBoris <Visuya@outlook.fr>
2022-03-31 03:17:35 +0200

    add profile

---
+++ frontend/src/pages/Profile/Profile.js
@@ -0,0 +1,80 @@
+import "./Profile.scss";
+import { Container, Row, Col } from "react-bootstrap";
+import BrandLogo from "../../globals/Header/BrandLogo";
+import SearchBar from "../../globals/Header/SearchBar";
+import Menu from "../../globals/Header/Menu";
+import FooterMenu from "../../globals/Footer/FooterMenu";
+import ProfileCard from "./components/ProfileCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ProfilePosts from "./components/ProfilePosts";
+
+function Profile() {
+  const userStorage = localStorage.getItem("user");
+  const user = JSON.parse(userStorage);
+  const [userProfile, setUserProfile] = useState({});
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://localhost:1331/api/auth/profile/${
+          window.location.pathname.split("/")[2]
+        }`,
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      )
+      .then((res) => {
+        setUserProfile(res.data);
+        setLoaded(true);
+      });
+  }, [user.token]);
+
+  return (
+    <Container fluid className="page-container profile">
+      <Row className="profile-header mb-1 pt-2">
+        <Col xs={6} sm={6} md={4} xl={3} className="d-flex logo-header">
+          <BrandLogo />
+        </Col>
+        <Col className="align-self-center search-bar">
+          <SearchBar md={4} xl={6} />
+        </Col>
+        <Col xs={6} sm={6} md={4} xl={3} className="menu">
+          <Menu />
+        </Col>
+      </Row>
+
+      <Row className="profile-main">
+        <Col xs={8} sm={8} md={3} className="profile-card">
+          <ProfileCard
+            profilePicture={userProfile.profile_picture}
+            name={`${userProfile.firstname} ${userProfile.lastname}`}
+            role={userProfile.company_role}
+            className="user-card"
+          />
+        </Col>
+        <Col
+          xs={12}
+          sm={12}
+          md={8}
+          xl={6}
+          className="px-5 posts-list"
+          id="postsList"
+        >
+          {loaded && <ProfilePosts posts={userProfile.posts} />}
+        </Col>
+      </Row>
+
+      <Row className="footer fixed-bottom">
+        <Col>
+          <FooterMenu />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Profile;
---
+++ frontend/src/pages/Profile/components/ProfilePosts.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import Post from "../../Homepage/components/Posts/Post";
+
+function ProfilePosts(props) {
+  const userStorage = localStorage.getItem("user");
+  const user = JSON.parse(userStorage);
+  const userPosts = props.posts;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    setPosts(userPosts);
+  }, [userPosts]);
+
+  const updateComments = (postId, comment) => {
+    let copyPosts = [...posts];
+    let post = copyPosts.find((p) => p.id === postId);
+    post.comments = post.comments.concat([comment]);
+    setPosts(copyPosts);
+  };
+
+  return (
+    <>
+      {posts.map((post) => (
+        <Post
+          key={post.id ? post.id : "dynamicKey"}
+          id={post.id}
+          authorId={post.userId}
+          postId={post.id}
+          profilePicture={
+            post.user ? post.user.profile_picture : user.profile_picture
+          }
+          author={
+            post.user
+              ? `${post.user.firstname} ${post.user.lastname}`
+              : user.author
+          }
+          description={post.description}
+          mediaUrl={post.mediaUrl}
+          commentsNumber={post.comments ? post.comments.length : "0"}
+          comments={post.comments ? post.comments : []}
+          likesNumber={!post.likes || post.likes === 0 ? "" : post.likes}
+          updateComments={updateComments}
+          userLiked={
+            post?.likedUsers?.find((u) => u.id === user.userId) ? true : false
+          }
+        />
+      ))}
+    </>
+  );
+}
+
+export default ProfilePosts;
